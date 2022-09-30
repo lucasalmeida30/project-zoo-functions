01adaafaf09614caed0c04cdb5f4141b315fd31b
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-09-30 19:32:29 -0300

    finalizado requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,13 +1,21 @@
 const data = require('../data/zoo_data');
+// const { employees } = data;
 
 function isManager(id) {
-  const { employees } = data;
-  const managers = employees.some((people) => people.managers === id);
+  const managers = data.employees.some((people) => people
+    .managers.some((element) => element === id));
   return managers;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  if (managerId) {
+    return data.employees.filter((element) => element
+      .managers.some((manager) => manager === managerId))
+      .map((element2) => `${element2.firstName} ${element2.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
