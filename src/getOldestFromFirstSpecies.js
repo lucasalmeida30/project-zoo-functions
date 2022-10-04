cde67e05053b40b024de01847f2994638491a3a5
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-10-04 11:23:29 -0300

    finalizado requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const people = data.employees.filter((element) => element.id === id);
+  const a = people.find((e) => e).responsibleFor[0];
+  const age = species.filter((b) => b.id === a)
+    .map((c) => c.residents.map((d) => d.age))[0].sort((x, b) => b - x)[0];
+  const r = species.find((element) => element.id === a).residents
+    .filter((element2) => element2.age === age)[0];
+  return Object.values(r);
 }
 
 module.exports = getOldestFromFirstSpecies;
