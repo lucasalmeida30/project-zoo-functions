b4aa0b4ac6bbabba7abe1b94f262ab5b4c599dea
lucasalmeida30 <lucas_almeida12@icloud.com>
2022-10-05 21:01:45 -0300

    refatorando requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -6,10 +6,8 @@ function getOldestFromFirstSpecies(id) {
   const people = data.employees.filter((element) => element.id === id);
   const a = people.find((e) => e).responsibleFor[0];
   const age = species.filter((b) => b.id === a)
-    .map((c) => c.residents.map((d) => d.age))[0].sort((x, b) => b - x)[0];
-  const r = species.find((element) => element.id === a).residents
-    .filter((element2) => element2.age === age)[0];
-  return Object.values(r);
+    .map((c) => c.residents.filter((d) => d.age))[0].sort((x, b) => b.age - x.age)[0];
+  return Object.values(age);
 }
-
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
